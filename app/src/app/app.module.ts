21b748156e8736a61a51c78ede89721705ee38d5
ESCOBAR-ANNER <aescobarc13@miumg.edu.gt>
2023-10-20 23:44:26 -0600

    subiendo estructura del proyecto

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login/login.component';
+import { PagesComponent } from './login/pages/pages.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagesComponent,
+    RegistroComponent,
+    AboutUsComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
