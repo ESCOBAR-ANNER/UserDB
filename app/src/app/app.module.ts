1c06e3821578a8ca442bf7576edb608184e6b31e
ESCOBAR-ANNER <aescobarc13@miumg.edu.gt>
2023-10-21 00:27:07 -0600

    subiendo v1

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,22 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
-import { PagesComponent } from './login/pages/pages.component';
+import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
+import { ConfigurationComponent } from './components/Configuracion/configuration.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagesComponent,
     RegistroComponent,
     AboutUsComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    ConfigurationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
